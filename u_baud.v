d10d7f63f4f4232005bcb23d3cba9c4d9120a5bf
Aayush-75 <aayushdesai070505@gmail.com>
2026-05-19 10:59:43 +0530

    Update u_baud.v

--- u_baud.v
+++ u_baud.v
@@ -1,4 +1,4 @@
-module u_baud#(parameter SYS_CLK_FREQ = 32, parameter BAUD_RATE = 1, parameter DATA_WIDTH=8, parameter PARITY_ENABLE=0)
+module u_baud#(parameter SYS_CLK_FREQ = 10000000, parameter BAUD_RATE = 115200, parameter DATA_WIDTH=8, parameter PARITY_ENABLE=0)
   (
     input sys_clk,
     input rst,
