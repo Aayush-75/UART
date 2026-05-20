704a0d89ad2f4b0c70716e833c1927c5720bb0d6
Aayush-75 <aayushdesai070505@gmail.com>
2026-05-20 16:59:11 +0530

    Add files via upload

---
+++ src/tb/dut/baud_gen.v
@@ -0,0 +1,31 @@
+`default_nettype none
+
+module baud_gen #(
+parameter BAUD = 2400,		// default baud rate = 2400 bits per sec
+parameter XTAL_CLK = 100_000_000,	// 100MHz
+parameter WORD_LEN = 8
+)(
+input wire sys_clk,
+input wire sys_rst_l, // active low reset signal
+output reg uart_clk
+);
+
+localparam integer CV = XTAL_CLK/(16*2*BAUD);	// count value
+localparam integer CW = $clog2(CV);	// counter width
+
+//counter reg for generating baud clock
+reg [CW:0] count;
+	
+always@(posedge sys_clk) begin
+	if(!sys_rst_l) begin
+		count <= 0;
+		uart_clk <= 0;
+	end
+	else if(count == CV) begin
+		uart_clk <= ~uart_clk;
+		count <= 0;
+	end
+	else
+		count <= count + 1;
+end
+endmodule
